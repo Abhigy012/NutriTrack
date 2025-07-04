53e7e8fdd3c73227e72d6d39b0979d70324ec6ba
Abhigy012 <abhygy454@gmail.com>
2025-07-04 20:18:13 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='min-h-screen w-full bg-slate-800 text-white text-shadow-amber-300 flex items-center justify-center'>
+      <h1 className='tracking-tight font-extrabold underline'>Working fine!</h1>
+    </div>
+  ) 
+}
+
+export default App
